39934878c595cd0523af212ca975dcf92449523a
Susmita Das <findsusmitadas18@gmail.com>
2025-06-08 12:27:49 +0530

    Initial Updated

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -21,18 +21,37 @@ function App() {
   }, [length, numberAllowed, charAllowed, setPasssword]);
 
   return (
-    <>
-    <div className="w-full text-2xl max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600">
-    <div className="w-full text-2xl max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600">
-    <input type="text"
-    value={password}
-    className="outline-none w-full py-1 px-3"
-    placeholder="password"
-    readOnly
-    />
-    </div>
-    </div>
-    </>
+    
+      <div className="w-full text-2xl max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-600">
+        <h1 className="text-white text-center my-4">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-4 py-1 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {e.setLength(e.target.value)}}
+            />
+            <label>Length: {length}</label>
+          </div>
+        </div>
+      </div>
+
   );
 }
 
