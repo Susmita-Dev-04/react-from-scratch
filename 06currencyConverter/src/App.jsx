44409fc21a2eb92b53b4916374b5998d1d639ef7
Susmita Das <findsusmitadas18@gmail.com>
2025-06-19 23:46:33 +0530

    Currency Converter mini project creating

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
     setConvertedAmount(amount * currencyInfo[to]);
   };
   return (
+    
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
