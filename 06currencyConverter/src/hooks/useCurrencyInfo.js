57663306424d61ed978c7f7b8d00cb07da270c7f
Susmita Das <findsusmitadas18@gmail.com>
2025-06-19 11:56:58 +0530

    custom react

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+//custom hook to fetch currency information
+
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    comst [data, setData] = useState({});
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((response) => response.json())
+        .then((response) => setData(response[currency]))
+        console.log(data);
+    },[currency])
+    console.log(data);
+    return data;    
+}
+
+export default useCurrencyInfo;
